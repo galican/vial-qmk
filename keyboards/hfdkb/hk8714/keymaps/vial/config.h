5a48686df0045c871cbd6de057093af82af66b53
galican <2831039915@qq.com>
2025-08-19 21:30:29 +0800

    Added new board hk8714.

---
+++ keyboards/hfdkb/hk8714/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0xE0, 0x2A, 0x96, 0x19, 0x56, 0xC4, 0x66, 0x96}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {3, 13}
